e9a1641b53e4be3a463f8941f847c2d2587d91fe
kadzoku <kadzoku@outlook.com>
2026-03-04 00:19:04 +0300

    registration test

--- src/test/java/tests/TextBoxtests.java
+++ src/test/java/tests/TextBoxtests.java
@@ -2,14 +2,15 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebElementCondition;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class TextBoxtests {
@@ -24,13 +25,32 @@ public class TextBoxtests {
     @Test
     void fillFormTests(){
         open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        //executeJavaScript("$('#fixedban').remove()");
+        //executeJavaScript("$('footer').remove()");
         $("#firstName").setValue("Имя");
         $("#lastName").setValue("Емейл");
+        $("#userEmail").setValue("test@mail.ru");
+        $("input[type=radio][value=Other]").click();
         $("#userNumber").setValue("89998886655");
-        $("#gender-radio-3").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbies-checkbox-2").click();
+        $("#currentAddress").setValue("sfsewfrewr");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__year-select").selectOptionByValue("1984");
+        $(".react-datepicker__day--007:not(.react-datepicker__day--outside-month)").click();
+        $("#react-select-3-input").setValue("Uttar Pradesh").pressEnter();
+        $("#react-select-4-input").setValue("Lucknow").pressEnter();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/img/001.png"));
         $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        //$(".table-responsive").shouldHave(text("89998886655"), text("test@mail.ru"));
         //Selenide.refresh();
 
 
     }
+
+
 }
