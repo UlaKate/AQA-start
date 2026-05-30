1113fc287b3916890772f0f7ba16507051111c70
kadzoku <kadzoku@outlook.com>
2026-05-31 00:43:09 +0300

    new

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 public class TestBase {
     @BeforeAll
     static void BeforeAll() {
-        Configuration.browserSize = "1700*450";
+        Configuration.browserSize = "1950*1700";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.holdBrowserOpen = true;
     }
