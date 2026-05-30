31f911d7475f46043c0187e34ac0e04c2d4a1204
kadzoku <kadzoku@outlook.com>
2026-05-31 00:37:11 +0300

    new

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 public class TestBase {
     @BeforeAll
     static void BeforeAll() {
-        Configuration.browserSize = "1400*450";
+        Configuration.browserSize = "1700*450";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.holdBrowserOpen = true;
     }
