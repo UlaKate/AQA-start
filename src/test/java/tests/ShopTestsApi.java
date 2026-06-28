c80ec08de079b5c1aafe86df4b0a8f7d757a943b
kadzoku <kadzoku@outlook.com>
2026-06-28 20:34:34 +0300

    api

---
+++ src/test/java/tests/ShopTestsApi.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class ShopTestsApi {
+     /*
+1. Make request (POST) to https://demoqa.com/Account/v1/GenerateToken  with body {"userName":"003","password":"Qaz_123!"}
+2. Get response
+  {"token": "string",
+  "expires": "2026-06-28T17:29:51.730Z",
+  "status": "string",
+  "result": "string"}
+3. Check "token" is "string" and status code 200
+  */
+}
--- src/test/java/tests/StatusTests.java
+++ src/test/java/tests/StatusTests.java
@@ -1,7 +1,10 @@
 package tests;
 
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
 
 public class StatusTests {
@@ -11,6 +14,46 @@ public class StatusTests {
         get("https://selenoid.autotests.cloud/status")
                 .then()
                 .body("total",is(20));
+    }
 
+    @Test
+    void checkTotalWithResponseLogs(){
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total",is(21));
+    }
+
+    @Test
+    void checkTotalWithLogs(){
+        given()
+                .log().all()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total",is(20));
+    }
+
+    @Test
+    void checkTotalWithSomeLogs(){
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().body()
+                .body("total",is(20));
+    }
+
+    @Test
+    void checkTotalWithStatusLogs(){
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total",is(20))
+                .body("browsers.chrome",hasKey("148.0"));
     }
 }
